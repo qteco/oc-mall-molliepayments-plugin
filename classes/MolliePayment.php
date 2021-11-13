qteco/oc-mall-molliepayments-plugin

commit ca04d353ad01862b3da9bf753744d22fac03e0eb

Redirect to correct page after completing order
The plugin now redirects to the correct "thank you" or "order failed" page after completing the Mollie payment.

Removed "Order page" settings field.

To-do:
The webhook is still required for expired payments, but for other types of payment statuses, the Mall payment status currently gets updated twice (2 db records, 2 e-mails etc).

Need to find a way to determine whether or not the webhook has updated the payment status yet to prevent changePaymentStatus() from being called twice.

classes/MolliePayment.php
@@ -4,6 +4,8 @@ namespace Qteco\MallMolliePayments\Classes;
 use OFFLINE\Mall\Classes\Payments\PaymentProvider;
 use OFFLINE\Mall\Classes\Payments\PaymentResult;
 use OFFLINE\Mall\Models\PaymentGatewaySettings;
+use Mollie\Api\MollieApiClient;
+use Mollie\Api\Exceptions\ApiException as MollieApiException;
 use OFFLINE\Mall\Models\Order;
 use Throwable;
 use Session;
@@ -58,12 +60,6 @@ class MolliePayment extends PaymentProvider
             "live_api_key" => [
                 "label" => "qteco.mallmolliepayments::lang.settings.live_api_key",
                 "comment" => "qteco.mallmolliepayments::lang.settings.live_api_key_label",
-                "span" => "left",
-                "type" => "text",
-            ],
-            "orders_page" => [
-                "label" => "qteco.mallmolliepayments::lang.settings.orders_page",
-                "comment" => "qteco.mallmolliepayments::lang.settings.orders_page_label",
                 "span" => "left",
                 "type" => "text",
             ],
@@ -123,6 +119,7 @@ class MolliePayment extends PaymentProvider
         }
 
         Session::put("mall.payment.callback", self::class);
+        Session::put("qteco.mallmolliepayments.orderReference", $payment->id);
 
         return $result->redirect($payment->getCheckoutUrl());
     }
@@ -136,7 +133,9 @@ class MolliePayment extends PaymentProvider
      */
     public function complete(PaymentResult $result): PaymentResult
     {
-        return $result->redirect(PaymentGatewaySettings::get("orders_page"));
+        $orderReference = Session::pull("qteco.mallmolliepayments.orderReference");
+
+        return self::changePaymentStatus($orderReference);
     }
 
     /**
@@ -146,11 +145,11 @@ class MolliePayment extends PaymentProvider
      *
      * @return PaymentResult
      */
-    public function changePaymentStatus($response): PaymentResult
+    public function changePaymentStatus($payment_id): PaymentResult
     {
         try {
             // Get payment data from Mollie using transaction ID from the webhook request
-            $payment = $this->getGateway()->payments->get($response->id);
+            $payment = $this->getGateway()->payments->get($payment_id);
 
             // Find the right order using the order number from the Mollie payment data
             $order = Order::where("order_number", $payment->metadata->order_number)->first();
@@ -170,7 +169,7 @@ class MolliePayment extends PaymentProvider
             } elseif ($payment->isCanceled()) {
                 return $result->fail((array) $payment, trans("offline.mall::lang.payment_status.cancelled"));
             }
-        } catch (\Mollie\Api\Exceptions\ApiException $e) {
+        } catch (MollieApiException $e) {
             Log::error("API call failed: " . htmlspecialchars($e->getMessage()));
         }
     }
@@ -178,9 +177,9 @@ class MolliePayment extends PaymentProvider
     /**
      * Build the payment gateway for Mollie
      *
-     * @return \Mollie\Api\MollieApiClient
+     * @return MollieApiClient
      */
-    protected function getGateway()
+    protected function getGateway(): MollieApiClient
     {
         $apiKey = null;
 
@@ -190,7 +189,7 @@ class MolliePayment extends PaymentProvider
             $apiKey = PaymentGatewaySettings::get("test_api_key");
         }
 
-        $gateway = new \Mollie\Api\MollieApiClient();
+        $gateway = new MollieApiClient();
         $gateway->setApiKey(decrypt($apiKey));
 
         return $gateway;
@@ -199,7 +198,7 @@ class MolliePayment extends PaymentProvider
     /**
      * Produce an array of active Mollie payment methods
      *
-     * @return array
+     * @return array An array of payment methods that are enabled in the Mollie Dashboard
      */
     protected function getActivePaymentMethods(): array
     {
@@ -211,7 +210,7 @@ class MolliePayment extends PaymentProvider
             foreach ($methods as $method) {
                 array_push($paymentMethods, $method->id);
             }
-        } catch (\Mollie\Api\Exceptions\ApiException $e) {
+        } catch (MollieApiException $e) {
             Log::error("API call failed: " . htmlspecialchars($e->getMessage()));
         }
 

routes.php
@@ -4,7 +4,7 @@ use Qteco\MallMolliePayments\Classes\MolliePayment;
 
 Route::post("/oc-mall-molliepayments-checkout", function (Request $request) {
     $molliePayment = new MolliePayment();
-    $molliePayment->changePaymentStatus($request);
+    $molliePayment->changePaymentStatus($request->id);
 
     return exit();
 });

lang/de/lang.php
@@ -11,8 +11,5 @@ return [
         "test_api_key_label" => 'Sieht aus wie "test_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"',
         "live_api_key" => "Mollie live API-Schlüssel",
         "live_api_key_label" => 'Sieht aus wie "live_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"',
-        "orders_page" => "Ihre Bestellseite",
-        "orders_page_label" =>
-            "Kunden werden nach dem Auschecken auf diese Seite weitergeleitet, Beispiel: http://yourwebsite.com/account/orders",
     ],
 ];

lang/en/lang.php
@@ -11,8 +11,5 @@ return [
         "test_api_key_label" => 'Looks like "test_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"',
         "live_api_key" => "Mollie live API key",
         "live_api_key_label" => 'Looks like "live_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"',
-        "orders_page" => "Orders page",
-        "orders_page_label" =>
-            "Customers will be redirected to this page after checkout, example: http://yourwebsite.com/account/orders",
     ],
 ];

lang/fr/lang.php
@@ -11,8 +11,5 @@ return [
         "test_api_key_label" => 'Ressemble à "test_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"',
         "live_api_key" => "Clé API de live de Mollie",
         "live_api_key_label" => 'Ressemble à "live_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"',
-        "orders_page" => "Page des commandes",
-        "orders_page_label" =>
-            "Les clients seront redirigés vers cette page après le paiement, exemple: http://yourwebsite.com/account/orders",
     ],
 ];

lang/nl/lang.php
@@ -11,8 +11,5 @@ return [
         "test_api_key_label" => 'Ziet er uit als "test_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"',
         "live_api_key" => "Mollie live API sleutel",
         "live_api_key_label" => 'Ziet er uit als "live_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"',
-        "orders_page" => "Bestellingen pagina",
-        "orders_page_label" =>
-            "Klanten worden na het afrekenen doorgestuurd naar deze pagina, bijvoorbeeld: http://yourwebsite.com/account/orders",
     ],
 ];

commit 32c987ba2d2ab55fa051765a658a24b54284ed98

Fix double payment status update
Fixes issue introduced in ca04d353ad01862b3da9bf753744d22fac03e0eb

classes/MolliePayment.php
@@ -119,7 +119,7 @@ class MolliePayment extends PaymentProvider
         }
 
         Session::put("mall.payment.callback", self::class);
-        Session::put("qteco.mallmolliepayments.paymentReference", $payment->id);
+        Session::put("qteco.mallmolliepayments.paymentId", $payment->id);
 
         return $result->redirect($payment->getCheckoutUrl());
     }
@@ -133,9 +133,9 @@ class MolliePayment extends PaymentProvider
      */
     public function complete(PaymentResult $result): PaymentResult
     {
-        $payment = Session::pull("qteco.mallmolliepayments.paymentReference");
+        $payment_id = Session::pull("qteco.mallmolliepayments.paymentId");
 
-        return self::changePaymentStatus($payment);
+        return self::changePaymentStatus($payment_id);
     }
 
     /**
@@ -145,7 +145,7 @@ class MolliePayment extends PaymentProvider
      *
      * @return PaymentResult An instance of PaymentResult with a changed payment status
      */
-    public function changePaymentStatus($payment_id): PaymentResult
+    public function changePaymentStatus(string $payment_id, bool $from_webhook = false): ?PaymentResult
     {
         try {
             // Get payment data from Mollie using transaction ID from the webhook request
@@ -160,15 +160,21 @@ class MolliePayment extends PaymentProvider
             $result = new PaymentResult($this, $order);
 
             // Update the order based on the payment status that Mollie has provided
-            if ($payment->isPaid()) {
-                return $result->success((array) $payment, trans("offline.mall::lang.payment_status.paid"));
-            } elseif ($payment->isFailed()) {
-                return $result->fail((array) $payment, trans("offline.mall::lang.payment_status.failed"));
-            } elseif ($payment->isExpired()) {
-                return $result->fail((array) $payment, trans("offline.mall::lang.payment_status.expired"));
-            } elseif ($payment->isCanceled()) {
-                return $result->fail((array) $payment, trans("offline.mall::lang.payment_status.cancelled"));
+            if ($from_webhook) {
+                if ($payment->isExpired()) {
+                    return $result->fail((array) $payment, trans("offline.mall::lang.payment_status.expired"));
+                }
+            } else {
+                if ($payment->isPaid()) {
+                    return $result->success((array) $payment, trans("offline.mall::lang.payment_status.paid"));
+                } elseif ($payment->isFailed()) {
+                    return $result->fail((array) $payment, trans("offline.mall::lang.payment_status.failed"));
+                } elseif ($payment->isCanceled()) {
+                    return $result->fail((array) $payment, trans("offline.mall::lang.payment_status.cancelled"));
+                }
             }
+
+            return null;
         } catch (MollieApiException $e) {
             Log::error("API call failed: " . htmlspecialchars($e->getMessage()));
         }

routes.php
@@ -4,7 +4,7 @@ use Qteco\MallMolliePayments\Classes\MolliePayment;
 
 Route::post("/oc-mall-molliepayments-checkout", function (Request $request) {
     $molliePayment = new MolliePayment();
-    $molliePayment->changePaymentStatus($request->id);
+    $molliePayment->changePaymentStatus($request->id, true);
 
     return exit();
 });